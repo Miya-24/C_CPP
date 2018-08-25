e3ed35eeff6b1fa18462651a730424848aa5ebeb
Miya-24 <37150304+Miya-24@users.noreply.github.com>
2018-08-25 15:15:55 +0900

    Update SCardCtrl.cpp

--- MyFelica/SCardCtrl.cpp
+++ MyFelica/SCardCtrl.cpp
@@ -1,10 +1,9 @@
 #include "SCardCtrl.h"
 #include "Main.h"
+#include "DeviceCtrl.h"
 
 MySmartCard::MySmartCard()
 {
-	this->hContext = 0;
-	this->lpszReaderName = nullptr;
 	this->hCard = 0;
 	this->dwActiveProtocol = 0;
 }
@@ -12,89 +11,6 @@ MySmartCard::MySmartCard()
 MySmartCard::~MySmartCard()
 {
 
-}
-//------------------------------------------
-BOOL MySmartCard::bOpenService()
-{
-	BOOL bRtn = TRUE;
-	bRtn = bConnectToSCResourceMng();
-	if (bRtn != TRUE) return FALSE;
-
-	bRtn = bSearchAndGetCRDeviceName();
-	if (bRtn != TRUE) return FALSE;
-
-	return TRUE;
-}
-//------------------------------------------
-void MySmartCard::vCloseService()
-{
-	if (this->hCard != 0) {
-		::SCardDisconnect(this->hCard, SCARD_LEAVE_CARD);
-	}
-	if (this->lpszReaderName != NULL) {
-		::SCardFreeMemory(this->hContext, this->lpszReaderName);
-	}
-	if (this->hContext != 0) {
-		::SCardReleaseContext(this->hContext);
-	}
-	system("pause");
-}
-//------------------------------------------
-BOOL MySmartCard::bConnectToSCResourceMng()
-{
-	SCARDCONTEXT	hContext = 0;
-	LONG lResult = ::SCardEstablishContext(SCARD_SCOPE_USER, NULL, NULL, &hContext);
-	if (lResult != SCARD_S_SUCCESS) {
-		if (lResult == SCARD_E_NO_SERVICE) {
-			fprintf_s(stdout, "Smart Card Servise is not Started.\n\n");
-		}
-		else {
-			fprintf_s(stdout, "SCardEstablishContext Error.\nErrorCode %08X\n\n", lResult);
-		}
-		return FALSE;
-	}
-	fprintf_s(stdout, "Smart Card Servise is Started.\n");
-	//set
-	this->hContext = hContext;
-	return TRUE;
-}
-//------------------------------------------
-BOOL MySmartCard::bSearchAndGetCRDeviceName()
-{
-	LPTSTR	lpszReaderNameTmp = NULL;
-	DWORD	dwAutoAllocate = SCARD_AUTOALLOCATE;
-
-	LONG lResult = ::SCardListReaders(this->hContext, NULL, (LPTSTR)&this->lpszReaderName, &dwAutoAllocate);
-	if (lResult != SCARD_S_SUCCESS) {
-		if (lResult == SCARD_E_NO_READERS_AVAILABLE) {
-			fprintf_s(stdout, "Reader/Writer is not Found.\n\n");
-		}
-		else {
-			fprintf_s(stdout, "SCardListReaders Error.\nErrorCode %08X\n\n", lResult);
-		}
-		return FALSE;
-	}
-	fprintf_s(stdout, "Reader/Writer is %s.\n\n", this->lpszReaderName);
-	if (int iRtn = memcmp(this->lpszReaderName, SONY_CR_DEVICE_NAME, strlen(SONY_CR_DEVICE_NAME)) != 0) {
-		return FALSE;
-	}
-	return TRUE;
-}
-//------------------------------------------
-BOOL MySmartCard::bConnectToSCard()
-{
-	SCARDHANDLE hCard = 0;
-	DWORD dwActiveProtocol = 0;
-	LONG lResult = ::SCardConnect(this->hContext, this->lpszReaderName, SCARD_SHARE_SHARED,
-									SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1, &hCard, &dwActiveProtocol);
-	if (lResult != SCARD_S_SUCCESS) {
-		fprintf_s(stdout, "SCardConnect Error.\nErrorCode %08X\n\n", lResult);
-		return false;
-	}
-	//set
-	this->dwActiveProtocol = dwActiveProtocol;
-	this->hCard = hCard;
-	return true;
 }
 //------------------------------------------
 BOOL MySmartCard::bGetRelatedData(BYTE cmd)
@@ -217,3 +133,19 @@ BYTE MySmartCard::byCheckCardType()
 	return byRcvBuf[0];
 }
 //------------------------------------------
+void MySmartCard::vDisConnectSCard()
+{
+	if (this->hCard != 0) {
+		::SCardDisconnect(this->hCard, SCARD_LEAVE_CARD);
+	}
+}
+//------------------------------------------
+void MySmartCard::vSetActiveProtocol(DWORD tmp)
+{
+	this->dwActiveProtocol = tmp;
+}
+//------------------------------------------
+void MySmartCard::vSetCardContxt(SCARDHANDLE tmp)
+{
+	this->hCard = tmp;
+}
