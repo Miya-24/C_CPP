bda7dcd6376117189808012772b20c370e76acd4
Miya-24 <37150304+Miya-24@users.noreply.github.com>
2018-08-25 15:15:29 +0900

    Create DeviceCtrl.h

---
+++ MyFelica/DeviceCtrl.h
@@ -0,0 +1,27 @@
+#ifndef DEVICECTRL_H_
+#define DEVICECTRL_H_
+
+#include <winscard.h>
+#include "SCardCtrl.h"
+#pragma comment (lib, "winscard.lib")
+
+class MyFelicaDevice {
+public:
+    MyFelicaDevice();
+    ~MyFelicaDevice();
+    BOOL bOpenService();
+	void vCloseService();
+    BOOL bConnectToSCResourceMng();
+	BOOL bSearchAndGetCRDeviceName();
+    BOOL bConnectToSCard();
+    void vDisConnectSCard();
+    SCARDCONTEXT hGetContext();
+    LPTSTR lpszGetReaderName();
+private:
+    SCARDCONTEXT hContext;
+    LPTSTR	     lpszReaderName;
+public:
+    MySmartCard* mysc;
+};
+
+#endif DEVICECTRL_H_
